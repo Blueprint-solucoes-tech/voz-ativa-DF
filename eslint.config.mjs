016c3a7bf3f010e57ef1f3910058d65107112cd0
gabrielbsb21 <gabrielbsb21@outlook.com>
2026-02-04 16:08:16 -0300

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    files: ["src/**/*.{test,spec}.{ts,tsx}"],
+    languageOptions: {
+      globals: globals.vitest,
+    },
+  },
+  prettier,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
